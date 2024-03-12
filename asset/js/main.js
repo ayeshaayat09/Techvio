48ad83db1d4f28a4acfb67a4c4bce4cd941863ac
ayeshaayat09 <islamsadia832@gmail.com>
2024-03-12 12:01:27 +0600

    header section add

---
+++ asset/js/main.js
@@ -0,0 +1,16 @@
+// navbar start
+const showMenuIcon = document.getElementById("show-icon");
+const hideMenuIcon = document.getElementById("hide-icon");
+const navbar = document.getElementById("navbar");
+function showMenu() {
+  navbar.style.display = "block";
+  navbar.style.top = "50px";
+  showMenuIcon.classList.add("d-none");
+  hideMenuIcon.classList.remove("d-none");
+}
+
+function hideMenu() {
+  navbar.style.display = "none";
+  hideMenuIcon.classList.add("d-none");
+  showMenuIcon.classList.remove("d-none");
+}
